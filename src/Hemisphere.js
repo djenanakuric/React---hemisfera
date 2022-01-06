dfa107a38c78dd233a4ee9d5bd8f30c44612867e
Djenana Kuric <76661983+djenanakuric@users.noreply.github.com>
2022-01-06 21:43:50 +0100

    Update Hemisphere.js

--- src/Hemisphere.js
+++ src/Hemisphere.js
@@ -2,14 +2,25 @@ import React from 'react';
 import North from './North.png';
 import South from './South.jpg';
 
+const hemisphereConfig = {
+    Southern: {
+        text: 'It is southern hemisphere!',
+        picture: South
+    },
+    Northern: {
+        text: 'It is northern hemisphere!',
+        picture: North
+    }
+}
 const HemisphereDisplay = (props) => {
 
-    const hemisphere = props.latitude > 0 ? 'Northern hemisphere!' : 'Southern hemisphere!';
-    const picture = props.latitude > 0 ? North : South;
+    const hemisphere = props.latitude > 0 ? 'Northern' : 'Southern';
+    const { text, picture } = hemisphereConfig[hemisphere];
+
     return (
         <div>
-            <img src={picture} alt="" />
-            {hemisphere}
+            <img src={picture} alt="hemisphere picture" />
+            {text}
         </div>
     )
 
